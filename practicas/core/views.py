3825b5d5a74429dcac4405a6517dfc80d7f23afa
Xtronksito <163474190+Xtronksito@users.noreply.github.com>
2024-10-17 14:18:04 -0300

    algunas validaciones

--- practicas/core/views.py
+++ practicas/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.http import JsonResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
 @login_required
@@ -10,3 +12,16 @@ def home(request):
 def salir(request):
     logout(request)
     return redirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Credenciales incorrectas.'})
+    
+    return render(request, 'login.html')
